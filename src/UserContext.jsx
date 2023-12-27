3effb167b16c2e863dfa72de585a201e179294e6
FilipeHibrael <filipehibrael27@gmail.com>
2023-12-26 22:48:05 -0300

    login criar

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -26,7 +26,6 @@ export function UserStorage({ children }) {
     const json = await response.json();
     setData(json);
     setLogin(true);
-    console.log(json);
   }
 
   async function userLogin(username, password) {
