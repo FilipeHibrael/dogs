56724c59615fec8f673e2ab5e5bb4987e3dd7578
FilipeHibrael <filipehibrael27@gmail.com>
2023-12-29 00:08:47 -0300

    comentario style

--- src/Components/Photo/PhotoComments.jsx
+++ src/Components/Photo/PhotoComments.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import { UserContext } from '../../UserContext';
 import PhotoCommentsForm from './PhotoCommentsForm';
-import styles from './PhotoComments.module.css'
+import styles from './PhotoComments.module.css';
 
 function PhotoComments(props) {
   const { login } = React.useContext(UserContext);
   const [comments, setComments] = React.useState(() => props.comments);
+  const commentsSection = React.useRef(null);
+
+  React.useEffect(() => {
+    commentsSection.current.scrollTop = commentsSection.current.scrollHeight;
+  }, [comments]);
 
   return (
     <>
-      <ul className={styles.comment}>
-        {comments.map((comment) => <li key={comment.comment_ID}>
-          <b>{comment.comment_author}: </b>
-          <span>{comment.comment_content}</span>
-        </li>)}
+      <ul ref={commentsSection} className={styles.comments}>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <b>{comment.comment_author}: </b>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
       </ul>
       {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
     </>
--- src/Components/Photo/PhotoCommentsForm.jsx
+++ src/Components/Photo/PhotoCommentsForm.jsx
@@ -3,6 +3,7 @@ import Envair from '../../Assets/enviar.svg?react';
 import useFetch from '../../Hooks/useFetch';
 import { COMMENT_POST } from '../../api';
 import Error from '../Helper/Error'
+import styles from './PhotoCommentsForm.module.css'
 
 function PhotoCommentsForm({ id, setComments }) {
   const [comment, setComment] = React.useState('');
@@ -19,15 +20,16 @@ function PhotoCommentsForm({ id, setComments }) {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <textarea
+      className={styles.textarea}
         id="comment"
         name="comment"
         placeholder="Comente..."
         value={comment}
         onChange={({ target }) => setComment(target.value)}
       />
-      <button>
+      <button className={styles.button}>
         <Envair />
       </button>
       <Error error={error}/>
